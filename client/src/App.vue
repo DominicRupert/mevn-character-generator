24cd12732700ea791ca5cd8d7025f58a72b855ae
dominic <dominicrupert@gmail.com>
2022-08-29 22:25:18 -0600

    adawdaw

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <button @click="toggle='character-viewer'">View All characters</button>
+    <button @click="toggle='character-creator'">Create Character</button>
+    <CharacterViewer v-show="toggle==='character-viewer'" />
+    <CharacterCreator v-show="toggle==='character-creator'" />
+  </div>
+</template>
+
+<script>
+import CharacterViewer from './components/CharacterViewer.vue'
+import CharacterCreator from './components/CharacterCreator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CharacterViewer,
+    CharacterCreator
+  },
+  data() {
+    return {
+      toggle: 'character-viewer'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
